b8b5597a7076bdc6b34fb4f1da464b5ca318a3ee
ho9science <full17@naver.com>
2020-06-11 00:18:09 +0900

    initial commit

--- apt_real.go
+++ apt_real.go
@@ -9,15 +9,15 @@ import (
 )
 type Response struct {
 	XMLName xml.Name `xml:"response"`
-	Header Header `xml: "header"`
-	Body Body `xml: "body"`
+	Header Header `xml:"header"`
+	Body Body `xml:"body"`
 }
 type Header struct{
-	ResultCode string `xml: "resultCode"`
-	ResultMsg string `xml: "resultMsg"`
+	ResultCode string `xml:"resultCode"`
+	ResultMsg string `xml:"resultMsg"`
 }
 type Body struct{
-	Items Items `xml: "items"`
+	Items Items `xml:"items"`
 }
 type Items struct{
 	Item []AptRealEstate `xml:"item"`
@@ -28,30 +28,30 @@ type Item struct {
 }
   
   type AptRealEstate struct {
-	ResultCode string `xml: "일련번호"`
-	RealYear string `xml: "년"`
-	DealAmount string `xml: "거래금액"`
-	BuildYear string `xml: "건축년도"`
-	RoadName string `xml: "도로명"`
-	RoadNameBonbun string `xml: "도로명본번호코드"`
-	RoadNameBunbun string `xml: "도로명건물부번호코드"`
-	RoadNameSigunguCode string `xml: "도로명시군구코드"`
-	RoadNameSeq string `xml: "도로명일련번호코드"`
-	RoadNameBasementCode string `xml: "도로명지상지하코드"`
-	RoadNameCode string `xml: "도로명코드"`
-	Dong string `xml: "법정동"`
-	Bonbun string `xml: "법정동본번코드"`
-	Bubun string `xml: "법정동부번코드"`
-	SigunguCode string `xml: "법정동시군구코드"`
-	EubmyundongCode string `xml: "법정동읍면동코드"`
-	LandCode string `xml: "법정동지번코드"`
-	ApartmentName string `xml: "아파트"`
-	DealMonth string `xml: "월"`
-	DealDay string `xml: "일"`
-	AreaForExclusiveUse float64 `xml: "전용면적"`
-	Jibun string `xml: "지번"`
-	RegionalCode string `xml: "지역코드"`
-	Floor int `xml: "층"`
+	ResultCode string `xml:"일련번호"`
+	RealYear string `xml:"년"`
+	DealAmount string `xml:"거래금액"`
+	BuildYear string `xml:"건축년도"`
+	RoadName string `xml:"도로명"`
+	RoadNameBonbun string `xml:"도로명본번호코드"`
+	RoadNameBunbun string `xml:"도로명건물부번호코드"`
+	RoadNameSigunguCode string `xml:"도로명시군구코드"`
+	RoadNameSeq string `xml:"도로명일련번호코드"`
+	RoadNameBasementCode string `xml:"도로명지상지하코드"`
+	RoadNameCode string `xml:"도로명코드"`
+	Dong string `xml:"법정동"`
+	Bonbun string `xml:"법정동본번코드"`
+	Bubun string `xml:"법정동부번코드"`
+	SigunguCode string `xml:"법정동시군구코드"`
+	EubmyundongCode string `xml:"법정동읍면동코드"`
+	LandCode string `xml:"법정동지번코드"`
+	ApartmentName string `xml:"아파트"`
+	DealMonth string `xml:"월"`
+	DealDay string `xml:"일"`
+	AreaForExclusiveUse float64 `xml:"전용면적"`
+	Jibun string `xml:"지번"`
+	RegionalCode string `xml:"지역코드"`
+	Floor int `xml:"층"`
   }
   
   func getXML(url string) ([]byte, error) {
@@ -76,9 +76,9 @@ type Item struct {
   func main(){
 
 	var url = "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?"
-	var serviceKey = "6n9eEV402Wxn5iBELz6R89uQRbB0tr5S2k23uKx%2F853VLXwdGY%2FlK%2B5Oo8W56xvG%2BtNe0NLj536ruGWjzkIAEQ%3D%3D"	
+	var serviceKey = ""	
 	var LAWD_CD = "11110"
-  	var DEAL_YMD = "201512"
+  	var DEAL_YMD = "200512" //200601
 	url = url+"LAWD_CD="+LAWD_CD+"&DEAL_YMD="+DEAL_YMD +"&serviceKey="+serviceKey
 	log.Printf(url)
 	if xmlBytes, err := getXML(url); err != nil {
@@ -89,8 +89,9 @@ type Item struct {
 		if err != nil {
 			log.Printf("error: %v", err)
 		}
-		for i := 0; i < len(result.Body.Items.Item); i++ {
-			log.Printf(result.Body.Items.Item[i].ResultCode)
+		var Items = result.Body.Items.Item
+		for i := 0; i < len(Items); i++ {
+			log.Printf(Items[i].ResultCode)
 		}
 	  }
 	}
