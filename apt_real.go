003b1d37c27752ddee037902e9b51a29f0b96024
ho9science <full17@naver.com>
2020-07-12 21:49:54 +0900

    로그 수정

--- apt_real.go
+++ apt_real.go
@@ -64,17 +64,17 @@ type Item struct {
   func getXML(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-	  return []byte{}, log.Fatalf("GET error: %v", err)
+	  return []byte{}, fmt.Errorf("GET error: %v", err)
 	}
 	defer resp.Body.Close()
   
 	if resp.StatusCode != http.StatusOK {
-	  return []byte{}, log.Fatalf("Status error: %v", resp.StatusCode)
+	  return []byte{}, fmt.Errorf("Status error: %v", resp.StatusCode)
 	}
   
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-	  return []byte{}, log.Fatalf("Read body: %v", err)
+	  return []byte{}, fmt.Errorf("Read body: %v", err)
 	}
   
 	return data, nil
